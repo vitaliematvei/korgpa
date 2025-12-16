c72c8e5f674d2eb559bd36806b5c1bd1f4f3bf61
Matvei Vitalie <vitaliematvei@gmail.com>
2025-12-16 10:48:39 +0200

    Disable CDN when using auth token to fix CORS

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,11 +1,11 @@
-import { createClient } from 'next-sanity'
+import { createClient } from 'next-sanity';
 
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, projectId } from '../env';
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: false, // Dezactivat când folosim token de autentificare
   token: process.env.SANITY_AUTH_TOKEN,
-})
+});
