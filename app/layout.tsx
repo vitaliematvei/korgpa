3203841aee9e55de7f266cb04ea6e13d970f4cc1
Matvei Vitalie <vitaliematvei@gmail.com>
2026-01-19 11:59:41 +0200

    am integrat chatul

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { Inter, Roboto_Mono } from 'next/font/google';
 import './globals.css';
+import Script from 'next/script';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { CartProvider } from './context/CartContext';
@@ -39,15 +41,52 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ro">
-      <body
-        className={`${inter.variable} ${robotoMono.variable} antialiased flex flex-col min-h-screen`}
-      >
-        <CartProvider>
-          <Header />
+    <html lang="en">
+      <body>
+        {children}
+        <Script
+          id="tawkto-widget"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/696dfd9fc0ef74197c0bf870/1jfaqddq3';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
+      </body>
+    </html>
+  );
           <main className="grow">{children}</main>
           <Footer />
         </CartProvider>
+
+        {/* Start of Tawk.to Script */}
+        <Script
+          id="tawkto"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/696dfd9fc0ef74197c0bf870/1jfaqddq3';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+      })();
+    `,
+          }}
+        />
+        {/* End of Tawk.to Script */}
       </body>
     </html>
   );
